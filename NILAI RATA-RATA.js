fdcd8f9d7cc06489d2fb7228b117f389f4af7d7a
Payriiz <ibnufarizfauzanhamzah7@gmail.com>
2025-09-25 10:41:06 +0700

    TUGAS SESI 1 (MENCARI NILAI RATA-RATA)

---
+++ NILAI RATA-RATA.js
@@ -0,0 +1,12 @@
+let matematika = 85;
+let bahasaInggris = 90;
+let ipa = 78;
+
+let jumlahSeluruh = matematika + bahasaInggris + ipa;
+let nilaiRataRata = jumlahSeluruh / 3;
+
+if (nilaiRataRata >= 80) {
+    console.log("LULUS DENGAN NILAI: " + nilaiRataRata)
+} else {
+    console.log("TIDAK LULUS DENGAN NILAI: " + nilaiRataRata)
+}
