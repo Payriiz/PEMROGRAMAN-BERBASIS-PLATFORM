5f048e0939eade4a9324800c7aaeafe1b7ad424f
Payriiz <ibnufarizfauzanhamzah7@gmail.com>
2025-10-10 15:14:22 +0700

    MATERI SESI 4

---
+++ MATERI/SESI 4.js
@@ -0,0 +1,64 @@
+// const userProfile = {
+//     //Properti dan Nilai
+//     firstName: "Budi",
+//     lastName: "Santoso",
+//     age: 30,
+//     email: "budi.santoso@example.com",
+//     isActive: true,
+
+//     //Metode (function sebagai properti)
+//     getFullName: function() {
+//         return this.firstName + " " + this.lastName;
+//     },
+    
+//     //Metode menggunakan shorthand ES6
+//     greet() {
+//         return "Halo, saya " + this.getFullName() + ".";
+//     }
+// };
+
+// //mengakses properti menggunakan dot notation
+// console.log("Nama Lengkap:", userProfile.getFullName());
+// console.log("Usia:", userProfile.age);
+// //mengakses properti menggunakan bracker notation
+// console.log("Status Aktif:", userProfile['isActive']);
+// console.log(userProfile.greet());
+
+// //mengubah nilai properti
+// userProfile.firstName = "Roni";
+// userProfile.lastName = "Sahroni";
+
+// //menambahkan properti baru
+// userProfile.nomorTelepon = "08123456789";
+// console.log("Nomor Telepon:", userProfile.nomorTelepon);
+
+// //menghapus properti
+// delete userProfile.lastName;
+
+
+// //contoh kedua
+// const keyPrefix = "id_";
+// const userId = 1001;
+// const keyDinamic = "alamat-user";
+
+// const userData = {
+//     [keyPrefix + "user"]: userId,
+//     [keyDinamic]: "Jl. Merdeka No. 10"
+// };
+
+// console.log(userData);
+
+//contoh ketiga
+function buatPemain(nama, skor) {
+    return {
+        nama,
+        skor,
+        tambahSkor(poin) {
+            this.skor += poin;
+        }
+    };
+}
+
+const pemain1 = buatPemain("Andi", 50);
+pemain1.tambahSkor(10);
+console.log(pemain1.skor); // Output: 60
