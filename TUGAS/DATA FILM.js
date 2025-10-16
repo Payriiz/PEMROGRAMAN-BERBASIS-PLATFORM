cbe21876cb6d224ec2354047b14d2c451ab5b25b
Payriiz <ibnufarizfauzanhamzah7@gmail.com>
2025-10-16 19:52:59 +0700

    TUGAS SESI 4 (DATA FILM)

---
+++ TUGAS/DATA FILM.js
@@ -0,0 +1,19 @@
+const dataFilm = {
+    judul: "Interstellar",
+    tahunRilis: 2014,
+    sutradara: "Christoper Nolan",
+    genre: ["Sci-Fi", "Adventure", "Drama"],
+
+    tampilkanDetail: function() {
+        return `Judul: ${this.judul}\nTahun Rilis: ${this.tahunRilis}\nSutradara: ${this.sutradara}\nGenre: ${this.genre.join(", ")}`;
+    }
+}
+
+const batas = "==============================================";
+console.log(batas);
+console.log(dataFilm.sutradara);
+console.log(batas);
+console.log(dataFilm.genre[1]);
+console.log(batas);
+console.log(dataFilm.tampilkanDetail());
+console.log(batas);
